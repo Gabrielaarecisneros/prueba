5d12bb0b3cf7a0881b28bfa92c5ab955f1ccdd59
GABRIELA <91220649+Gabrielaarecisneros@users.noreply.github.com>
2023-05-11 19:59:49 +0200

    Add files via upload

---
+++ Nuevo_box/src/nuevo_box/Nuevo_box.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nuevo_box;
+import java.sql.DriverManager;
+/**
+ *
+ * @author DAW
+ */
+public class Nuevo_box {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[]throws SQLException  {
+
+try{
+    Class.forName("com.mysql.cj.jdbc,Driver")newInstanee();
+    if (conexion.isClosed)){
+    System.out.println("jdbc:mysq://10.230.10");
+    if (conexion.isClosed()){
+        System.out.println("no se ha establesido la conexion");
+    }
+            Statement ejecu
+}
+// TODO code application logic here
+    }
+    
+}
